ffb75f304827d78c4ccd790ceb90cd4bef53b476
producao02 <producao02@twoweb.com.br>
2015-04-23 10:57:43 -0300

    Implementation of JQuery Maphilight

---
+++ lib/maphilight-rails.rb
@@ -0,0 +1,8 @@
+require "maphilight-rails/version"
+
+module Maphilight
+  module Rails
+    class Engine < ::Rails::Engine
+  	end
+  end
+end
---
+++ lib/maphilight-rails/version.rb
@@ -0,0 +1,5 @@
+module Maphilight
+  module Rails
+    VERSION = "1.0.0"
+  end
+end
